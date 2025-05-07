2d3a1852b2dc66957171dada3f9e8ebab57e66be
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-05-07 17:49:29 -0400

    learning more

--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -1,2 +1,3 @@
+# This is my first python program
 print("Hello World")
 print("I like learning Python")
---
+++ Lesson_2/main.py
@@ -0,0 +1,25 @@
+# Variable is a container for a value, strings, integers, floats, booleans
+
+#These are strings
+first_name = "Will"
+favorite_food = "Pizza"
+email = "will123@fake.com"
+
+#These are integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+#These are floats
+
+price = 10.99
+gpa = 3.98
+
+print(first_name)
+print(f"Hello {first_name}")
+print(f"You like {favorite_food}")
+print(f"Your email is {email}")
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"The price is ${price}")
+print(f"Your gpa is {gpa}")
