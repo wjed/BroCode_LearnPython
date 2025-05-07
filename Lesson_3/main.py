a6169adb902db9aa22a3ea7b93303ef3fd326af1
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-05-07 18:05:25 -0400

    right back in the a

---
+++ Lesson_3/main.py
@@ -0,0 +1,27 @@
+name = "Warning"
+age = 18
+gpa = 1.9
+student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+
+age = float(age)
+print(type(age))
+
+gpa = int(gpa)
+print(type(gpa))
+
+student = str(student)
+print(type(student))
+
+age = bool(age)
+print(age)
+
+x = 2
+y = 2.0
+
+x = x / y
+print(x)
---
+++ Lesson_4/main.py
@@ -0,0 +1,4 @@
+username = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello {username}")
+print(f"You are {age} years old")
